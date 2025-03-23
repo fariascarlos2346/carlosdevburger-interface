dfbaeea40a65be0ea513313ab6b00a2791a4483c
Carlos <fariascarlos2346@gmail.com>
2025-03-23 11:21:09 -0300

    estilos

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-import { Login } from './container/Login'
+import { Login } from './container/Login';
+import GlobalStyles from './styles/globalStyles';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Login/>
+    <GlobalStyles/>
   </StrictMode>,
-)
+);
