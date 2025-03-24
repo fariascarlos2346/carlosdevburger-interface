8dd9ba3194ab04ae64afe67d719ecf0e091894a7
Carlos <fariascarlos2346@gmail.com>
2025-03-24 08:57:39 -0300

    criando no react e importando no  styles.components

--- src/container/Login/index.jsx
+++ src/container/Login/index.jsx
@@ -1,7 +1,32 @@
+import { Button, Container, Form, InputContainer, LeftContainer, Link, RightContainer, Title } from './styles';
+import Logo from '../../assets/logo.svg';
+
 export function Login() {
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
+        <Container>
+            <LeftContainer>
+                <img src={Logo} alt='logo-dev'/>
+            </LeftContainer>
+            <RightContainer>
+                <Title>
+                    Olá, seja bem vindo ao <span>Dev Burger!</span> 
+                    Acesse com seu <span>Login e Senha.</span>
+                </Title>
+                <Form>
+                    <InputContainer>
+                        <labal>Email</labal>
+                        <input type="email"/>
+                    </InputContainer>
+
+                    <InputContainer>
+                        <labal>Senha</labal>
+                        <input type="password"/>
+                    </InputContainer>
+                    <Link>Esqueci minha senha.</Link>
+                    <Button>Entrar</Button>
+                </Form>
+                <Link>não possui conta?</Link>
+            </RightContainer>
+        </Container>
     );
 }
